c6067ba0f07bafd35d0d455ef61cc2a8a7747180
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-06-12 15:12:07 -0700

    herosec done

--- components/HeroSec.tsx
+++ components/HeroSec.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
+import Image from "next/image";
+
 
 export default function HeroSec() {
   return (
@@ -13,87 +16,65 @@ export default function HeroSec() {
 
             <div className="mt-8 space-y-10">
               <p className="flex items-center -mx-2 text-gray-800 ">
-                {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mx-2 text-blue-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg> */}
-
-                <span className="mx-2">Anyone can beat you but no one can beat your outfit as long as you wear 
-                Dine outfits.Anyone can beat you but no one can beat your outfit as long as you wear Dine outfits.</span>
+                <span className="mx-2">Anyone can beat you but no one can beat your outfit as long as you 
+                wear Dine outfits.</span>
               </p>
-
-              {/* <p className="flex items-center -mx-2 text-gray-700 ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mx-2 text-blue-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-
-                <span className="mx-2">Just Copy Paste Codeing</span>
-              </p>
-
-              <p className="flex items-center -mx-2 text-gray-700 ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mx-2 text-blue-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-
-                <span className="mx-2">Easy to Use</span>
-              </p> */}
             </div>
           </div>
 
-          <div className="w-full mt-8 bg-transparent border rounded-md lg:max-w-sm dark:border-gray-700 focus-within:border-blue-400 focus-within:ring focus-within:ring-blue-300 dark:focus-within:border-blue-400 focus-within:ring-opacity-40">
-            <form className="flex flex-col lg:flex-row">
-              <input
-                type="email"
-                placeholder="Enter your email address"
-                className="flex-1 h-10 px-4 py-2 m-1 text-gray-700 placeholder-gray-400 bg-transparent border-none appearance-none  focus:outline-none focus:placeholder-transparent focus:ring-0"
-              />
-
+          <div className=" mt-8 lg:max-w-xs dark:border-gray-700 focus-within:border-blue-400 
+          focus-within:ring focus-within:ring-blue-300 dark:focus-within:border-blue-400 
+          focus-within:ring-opacity-40 ">
+            
+              <div className="w-full flex justify-center gap-3 font-semibold py-4 
+               text-white transition-colors duration-200 transform bg-black hover:bg-blue-400 
+               focus:outline-none focus:bg-blue-400">
+              <FiShoppingCart className="w-8 h-8 flex justify-center items-center text-white cursor-pointer"/>
               <button
                 type="button"
-                className="h-10 px-4 py-2 m-1 text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400"
+                className="text-xl"
               >
-                Join Us
+                Start Shopping
               </button>
-            </form>
+              </div>
+              <div className="flex w-full gap-20 pt-24">
+                
+            <Image 
+            src='/images/featured1.png'
+            alt="feature"
+            width={500}
+            height={500}
+            
+            />
+            
+             <Image 
+            src='/images/Featured2.png'
+            alt="feature"
+            width={500}
+            height={500}
+            />
+             <Image 
+            src='/images/Featured3.png'
+            alt="feature"
+            width={500}
+            height={500}
+            />
+             <Image 
+            src='/images/Featured4.png'
+            alt="feature"
+            width={500}
+            height={500}
+            />
           </div>
+          </div>
+          
         </div>
 
-        <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
+        <div className="flex items-center justify-center w-full h-full lg:w-1/2">
+            <div className="relative bg-[#FFECE3] lg:p-72 mr-14 rounded-full"></div>
           <img
-            className="object-cover w-full h-full mx-auto rounded-md lg:max-w-2xl"
-            src="https://images.unsplash.com/photo-1543269664-7eef42226a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            className="absolute object-cover mx-auto rounded-md "
+            src="/images/header.png"
             alt="glasses photo"
           />
         </div>
---
+++ components/Promotion.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Image from "next/image";
+
+
+export default function Promotion() {
+  return (
+    <div className="mx-auto bg-[#ffff] space-y-10">
+      <div className="flex flex-col justify-center items-center space-y-5">
+        <p className="text-sm font-semibold text-blue-600 pt-10">PROMOTIONS</p>
+
+        <div className="text-4xl font-semibold text-black">
+          Our Promotions Events
+        </div>
+      </div>
+      <div className="w-full pb-5 flex container mx-auto">
+       <div className="w-full lg:w-8/12 px-8">
+        <div className="flex flex-col space-y-3">
+        <div className="flex justify-between items-center bg-[#D6D6D8] px-10">
+            <div className="flex flex-col">
+                <h2 className="text-3xl font-bold text-black">GET UP TO 60%</h2>
+                <h5 className="text-black">For the summer season</h5>
+            </div>
+            <div className="w-60">
+                <Image
+                src='/images/event1.png'
+                alt="feature"
+                width={500}
+                height={500}
+                />
+            </div>
+        </div>
+
+        <div className="flex justify-center items-center bg-[#212121] p-7 text-white">
+            <div className="flex flex-col justify-center items-center space-y-3">
+                <h2 className="text-3xl font-bold">GET 30% Off</h2>
+                <div className="space-y-1">
+                <h5 className="flex justify-center">USE PROMO CODE</h5>
+               <div className="bg-[#474747] rounded-full px-10 text-white font-semibold text-lg text-justify">
+                <button className="py-1 ">DINEWEEKEND SALE</button>
+                </div> 
+                </div>
+            </div>
+            
+        </div>
+        </div>
+       </div>
+       <div className="w-full lg:w-2/12 ">
+        <div className="bg-[#EFE1C7] text-gray-700">
+            <div className="flex flex-col p-1">
+                <p>Flex Sweatshirt</p>
+                <div className="flex gap-2">
+                <p>$100.00</p>
+                <p className="font-semibold">$75.00</p>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <Image
+                src='/images/event2.png'
+                alt="feature"
+                width={500}
+                height={500}
+                className="w-60"
+                />
+            </div>
+        </div>
+       </div>
+       <div className="w-full lg:w-2/12 px-1">
+       <div className="bg-[#D7D7D9] text-gray-700">
+            <div className="flex flex-col p-1">
+                <p>Flex Push Button Bomber</p>
+                <div className="flex gap-2">
+                <p>$1225.00</p>
+                <p className="font-semibold">$190.00</p>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <Image
+                src='/images/event3.png'
+                alt="feature"
+                width={500}
+                height={500}
+                className="w-60"
+                />
+            </div>
+        </div>
+       </div>
+      </div>
+    </div>
+  );
+}
